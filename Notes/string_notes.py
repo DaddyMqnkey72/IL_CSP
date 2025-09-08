72a496312465b0de6c8c6dac6b46b2a93c08a381
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-08 14:41:47 -0600

    notes

--- Notes/string_notes.py
+++ Notes/string_notes.py
@@ -1,1 +1,25 @@
-#Il 7th string notes
+#IL 7th string notes
+
+
+#what makes someything  string?
+    #letters or numbers hed together with qoutation marks
+name = input("what is your name?").strip().title()
+sentence = "the quick brown fox jumps over the lazy dog"
+print ("welcome",name)
+freak = name + sentence
+print ("welcome" , freak)
+#why do we have strings?
+
+#how do strupid proofing and sanitazation relate to each other?
+
+#what is debugging?
+
+#HOw do you debug the different types of errors?
+
+#describe what each of the methods below does
+
+#find()
+#concatenate (add)
+#upper ()
+#lower ()
+#strip ()
