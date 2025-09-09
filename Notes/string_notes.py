08a5c2465ee28aa47a19ef778e36bc9f92d7d16c
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-09 14:40:48 -0600

    note finished

--- Notes/string_notes.py
+++ Notes/string_notes.py
@@ -9,17 +9,36 @@ print ("welcome",name)
 freak = name + sentence
 print ("welcome" , freak)
 #why do we have strings?
+#sanitaztion and stupid proofing
+    #making it look good and how you want it to look, stupid proofing is making sure the user cant break your code
 
 #how do stupid proofing and sanitazation relate to each other?
+    #they both make sure the user cant break your code and make it look good
 
 #what is debugging?
+    #debugging is fixing code that has errors in it
 
 #How do you debug the different types of errors?
+    #syntax error
+#error ="this is a error"
+    #logic error
+numone= "7"
+numtwo= "5"
+print (int(numone)+int(numtwo))
+    #runtime error
+
+num = 3
+print("num favorote number is", num)
 
 #describe what each of the methods below does
 
 #find()
+    #finds the index of a letter in a string
 #concatenate (add)
+     # puts the string together
 #upper ()
+    #makes all the letters uppercase
 #lower ()
-#strip ()
+    #makes all the letters lowercase
+#strip ()   
+    #removes spaces before and after the string
