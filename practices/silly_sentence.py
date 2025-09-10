88007b84e05581499bb1842f1b13420d3ea96658
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-10 14:40:33 -0600

    did some silly sentences

---
+++ practices/silly_sentence.py
@@ -0,0 +1,7 @@
+#IL 7th silly sentence
+
+
+adj=input("give me an adjective: ").strip().lower()
+transformers = input( "give me a transformer: ").strip().title()
+print("trandfomers is a " + adj + " robot that fights", transformers)
+
