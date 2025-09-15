4f1fd43b4d9a17e35789e58f7d5d2ccee7c3c0df
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-09-15 02:50:12 +0000

    did silly sentences

--- practices/silly_sentence.py
+++ practices/silly_sentence.py
@@ -3,5 +3,12 @@
 
 adj=input("give me an adjective: ").strip().lower()
 transformers = input( "give me a transformer: ").strip().title()
-print("trandfomers is a " + adj + " robot that fights", transformers)
+print("transfomers is a " + adj + " robot that fights", transformers)
 
+adj=input ("give me and adjective: ").strip(). lower()
+dragon=input ("give me a cool dragon name:").strip().title()
+print("dragon is a", adj, "creature that flies")
+
+adj=input("give me an adjective:").strip().lower()
+boxer=input("give me a cool boxer:").strip().lower()
+print("boxer is a", adj, "who fights for career")
