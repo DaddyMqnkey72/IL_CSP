7710ff32dddfd0b422b7e0719f085551d6b07ab4
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-16 14:41:55 -0600

    hello world update

---
+++ practices/hello_world_update.py
@@ -0,0 +1,11 @@
+#IL 7th hello world update
+
+def hello(name):
+    print (f'hello{name}')
+name=input("what is your name? \n")
+hello(name)
+hello("Alice")
+hello("Bob")
+hello("Eve")
+hello("isaiah")
+hello("enzo")
