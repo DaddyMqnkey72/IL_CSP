1e46fa4612a89a185951e91fa5c441011c571660
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-17 14:41:43 -0600

    notes

---
+++ Notes/conditional_notes.py
@@ -0,0 +1,35 @@
+#IL 7th conditional notes
+
+#What puts something inside of the “if” statement?
+
+homework_done = "no"
+
+if homework_done == "yes":
+    print("than do your homework!")
+else: 
+    print("yes you can go!")
+
+grade = 100
+if grade > 89:
+    print(f"you have {grade}%, you get an A!")
+elif grade > 79:
+    print(f"you have {grade}%, you get a B!")
+else:
+    print(f"you have {grade}%, you get a C or lower!")
+#what do statements do?
+   #They check if something is true or false
+#what are boolean statements? 
+   #they are always true or false
+#What do else statements do?
+    # iif its not true. thats what it says
+#What kind of statement do you use if you have more than 2 needed outcomes?
+#What do each of the different symbols mean in conditionals?
+    #< less than
+    #> greater than
+    #<=less than or equal to
+    #>=greater than or equal to
+    #== question "are they equal?"
+    #! not equal to
+#What are the 3 logical operators?
+#What are logical operators for?
+#What does a nested conditional statement do?
