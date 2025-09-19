9626b065315546758dfa15bb76e456f9f9bbf3c8
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-09-19 19:34:26 +0000

    started hw

---
+++ practices/time.py
@@ -0,0 +1,30 @@
+#IL 7th time of 
+
+time = int(input("hi what time is it amke sure to list time in military time"))
+
+if
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
+elif
