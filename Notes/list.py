cd483e1de30c363a3521c09dc0679d24d37ed147
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-22 14:40:35 -0600

    loops notes

--- Notes/list.py
+++ Notes/list.py
@@ -1,3 +1,10 @@
 #IL 7th lists notes
 
 sibling = ["isaiah,joceylyn, selena, ximena"]
+
+print(sibling)
+sibling[0] = "isaiah"
+sibling[2] = "joceylyn"
+sibling.remove ("selena")
+print(sibling)
+
---
+++ Notes/loops.py
@@ -0,0 +1,28 @@
+#IL 7th loops notes
+
+foods = ["pasta", "yogurt", "chicken", "rice", "beans"]
+
+#What is a loop?
+   # repeat the same code over and over again until a certain condition is met
+#What are the two types of loops?
+   # for loops and while loops
+for food in foods:
+     print(f"{food} is my favorite food")
+    
+for x  in range(1,20):
+    print(x)
+
+   #3 steps of a while loop, 1: iterator, how many times a loop runs, 2: stoppping point, 3: increase the iterator
+i=0
+while True:
+     print(i)
+     i += 1
+
+#What is iteration
+   # one loop of the same code
+#What are lists? 
+   # a collection of items in a particular order
+#How do you make lists in python? 
+   #
+#How do you make for loops in python? 
+#How do you make while loops in python?
