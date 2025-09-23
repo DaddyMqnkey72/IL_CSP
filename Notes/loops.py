dab1ee5845e42bedd1a97ac9cac9698ab44de279
DaddyMqnkey72 <Isaiah.lozano@ucas-edu.net>
2025-09-23 14:13:31 -0600

    finished family hw

--- Notes/loops.py
+++ Notes/loops.py
@@ -23,6 +23,8 @@ while True:
 #What are lists? 
    # a collection of items in a particular order
 #How do you make lists in python? 
-   #
+   # use square brackets []
 #How do you make for loops in python? 
+   # for item in list:
 #How do you make while loops in python?
+   # while condition:
---
+++ practices/my_family.py
@@ -0,0 +1,5 @@
+#IL 7th my family loop
+friends = ["Israel", "Rohann", "Zoey", "Juan", "Arsh", "Enzo"]
+
+for friend in friends:
+    print(f"hello {friend}")
