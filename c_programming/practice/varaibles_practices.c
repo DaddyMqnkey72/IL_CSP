59aa671d7603dbd55fd037ecff578a5c52c4cbba
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-07 01:49:05 +0000

    finsihsd hw

--- c_programming/practice/varaibles_practices.c
+++ c_programming/practice/varaibles_practices.c
@@ -1,1 +1,35 @@
 //Il 7th variables practices
+
+#include <stdio.h>
+
+int min (void){
+    char name []= "Izzy Lozano";
+    int number = 7;
+    float gpa = 3.2;
+    char breakfast[]="bacon";
+    char favorite_color []= "purple";
+    char school_name[]="ucas";
+    int year = 2025;
+    char eye_color[]="brown";
+    int age =14;
+    char favorite_subject[]="study hall";
+    double long_pi=3.141592653; 
+
+    printf( "name: %s\n",name); 
+    printf("number (1-10):%d\n",number); 
+    printf("gpa: %.2f\n", gpa);
+    printf("breakfast: %.2\n", breakfast);
+    printf("favorite color: %s\n",favorite_color);
+    printf("school name:%s\n", school_name);
+    printf("year:%d\n", school_name);
+    printf("eye color:%s\n", eye_color);
+    printf("age: %d\n", year);
+    printf("favorite subject: %s\n", favorite_subject);
+    printf("long pi: %.9fn", long_pi);
+
+
+
+
+return 0;
+
+}
