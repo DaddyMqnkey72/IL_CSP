96b3aa146a857375e42cb430292b6fd3e7ffd293
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-10 14:39:52 -0600

    started some of my old enough and time hw

---
+++ c_programming/practice/old_enough.c
@@ -0,0 +1,19 @@
+//IL 7th old enough
+
+#include <stdio.h>
+
+int main(void){
+    int age; 
+    printf("what is your age?");
+    scanf("%d", age);
+
+    if{(age >= 18)
+    } 
+
+
+
+
+
+
+    return 0; 
+}
