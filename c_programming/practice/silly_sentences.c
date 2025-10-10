d86dd26975e4bae323b110726a5d0a49c588581c
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-10 01:09:09 +0000

    so silly

--- c_programming/practice/silly_sentences.c
+++ c_programming/practice/silly_sentences.c
@@ -1,16 +1,23 @@
 //IL 7th silly snetences 
-#include <stdio.h>
+#include <stdio.h>// 
 
-int main (void){
+char celebrity[50];
+char restaurant[50];
+char food[50];
 
+int main(void){
 
+    printf("Welcome a silly sentences maker! Answer the following questions! (please only one word answers!!)\n");
 
+    printf("Who is you favorite celebrity?\n");
+    scanf("%s", celebrity);
+    printf("What is your favorite place to eat?\n");
+    scanf("%s", restaurant);
+    printf("What is you favorite food?\n");
+    scanf("%s", food);
+    
 
-
-
-
-
-
+    printf("You were on a run when %s saw you and you guys ran to %s and ate %s", celebrity, restaurant, food);
+    
     return 0;
 }
-
