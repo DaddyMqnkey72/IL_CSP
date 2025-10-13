7adf9d41341119fb7ee172bbe2b2c800317c8a77
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-13 02:13:24 +0000

    finishrf w

--- c_programming/practice/old_enough.c
+++ c_programming/practice/old_enough.c
@@ -1,19 +1,24 @@
 //IL 7th old enough
-
 #include <stdio.h>
+int num;
 
 int main(void){
-    int age; 
-    printf("what is your age?");
-    scanf("%d", age);
-
-    if{(age >= 18)
-    } 
-
-
-
-
-
-
-    return 0; 
+    printf("How old are you?\n");
+    scanf("%d", &num);
+if(num == 18){
+    printf("you can vote\n");
+}else if (num ==16){
+    printf("you can drive!\n");
+}else if (num == 15){
+    printf("You can get a learners permit\n");
+}else if (num ==4)
+    printf("you can go to school\n");
+else{
+    printf("you aren't any of these ages\n");
 }
+return 0;
+}
+
+
+
+
