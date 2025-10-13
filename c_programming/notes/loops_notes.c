36de5c60e2f55f74fb32917c930f583d0e5fdd14
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-13 14:17:49 -0600

    notes of thy loops

---
+++ c_programming/notes/loops_notes.c
@@ -0,0 +1,58 @@
+// IL 7thloops notes
+#include <stdio.h> 
+
+int main(void){
+    int nums[]={1,654,4,56,8,742,5}; // arrays
+    char candies[][20]={"kitkat","skittles","nerds","hershey","Reeses"};
+
+    for(int x = 0; x < 7; x++){
+        printf("%d\n", nums[x]);
+    }
+
+    for (int i=0; i< 5; i++){
+        printf("%s, is my favorite candy\n", candies[i]);
+    }
+    for(int num = 1; num < 11; num++){
+        printf("%d\n", num);
+
+    }
+
+    int num=0;
+    while(num<11){
+        printf("%d\n", num);
+        num++;
+    }
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//What is a loop? 
+    //a running sequence 
+//What are the two types of loops?
+    //
+//What is iteration
+    //one time through a loop
+//What are arrays? 
+    //list
+//How do you make lists in C? 
+    // using {}
+//How do you make for loops in C? 
+    //
+//How do you make while loops in C?
+    //
