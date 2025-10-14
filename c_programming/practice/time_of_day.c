52a4d95eb697935e83823029f80098901e615dcc
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-14 00:00:17 +0000

    fin time of da day hw

--- c_programming/practice/time_of_day.c
+++ c_programming/practice/time_of_day.c
@@ -1,10 +1,21 @@
 //Il 7th time of day
 #include <stdio.h>
 
-int main (void){
 
 
+int main(void) {
+    int time;
 
+    printf("What is the time in military time?: ");
+    scanf("%d", &time);
+
+    if (time < 12) {
+        printf("Good Morning!\n");
+    } else if (time < 18) {
+        printf("Good Afternoon!\n");
+    } else {
+        printf("Good Evening!\n");
+    }
 
     return 0;
 }
