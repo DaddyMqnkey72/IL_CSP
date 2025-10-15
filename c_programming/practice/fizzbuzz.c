75ffbc1ad02c3175767baa0dfe1a28d903a3b63a
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-15 00:32:42 +0000

    fizzbuzz hw

---
+++ c_programming/practice/fizzbuzz.c
@@ -0,0 +1,17 @@
+//IL 7th fizzbuzz
+#include <stdio.h>
+
+int main(){
+    for (int x = 1; x <= 50; x++){
+        if (x % 3 == 0 && x % 5 == 0) {
+            printf("fizzbuzz\n");
+        } else if (x % 3 == 0) {
+            printf("fizz\n");
+        } else if (x % 5 == 0) {
+            printf("buzz\n");
+        } else { 
+            printf("%d\n", x);
+        }
+    }
+    return 0;
+}
