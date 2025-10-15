63ec38502c6a8aa23d46e9e77d855b366c7425f9
DaddyMqnkey72 <isaiah.lozano@ucas-edu.net>
2025-10-15 00:35:45 +0000

    hw

--- c_programming/practice/family_loop.c
+++ c_programming/practice/family_loop.c
@@ -1,10 +1,13 @@
 //Il 7th 
 #include <stdio.h>
+#include <string.h>
+                
 
-int main(void){
-char family [][40] ={"israel","zoey","Rohann","juan","Gyan" };
-for(int x 0; x <5; );
+int main(void) {
+    char family[5][20] = {"Zoey", "Israel", "Juan", "Rohann", "Gyan"};
 
-
+    for (int i = 0; i < 5; i++) {
+        printf("Hello, %s!\n", family[i]);
+    }
     return 0;
 }
